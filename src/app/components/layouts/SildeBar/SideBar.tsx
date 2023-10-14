a0c314ddf0ed1bf2df81bba236ff0d90aa570fcc
quocbao <hoangquocbao650@gmail.com>
2023-10-14 12:14:24 +0700

    update

--- src/app/components/layouts/SildeBar/SideBar.tsx
+++ src/app/components/layouts/SildeBar/SideBar.tsx
@@ -1,34 +1,95 @@
 import Image from "next/image";
 import React from "react";
-import chatGPTLogo from "../../../../../public/ChatGPT_logo.svg.png";
+import chatGPTLogo from "../../../../../public/chatgpt.svg";
+
+import addBtn from "../../../../../public/add-30.svg";
+import msgIcon from "../../../../../public/message.svg";
+import home from "../../../../../public/home.svg";
+import saved from "../../../../../public/bookmark.svg";
+import rocked from "../../../../../public/rocket.svg";
+import sendBtn from "../../../../../public/send.svg";
+import userIcon from "../../../../../public/user.jpg";
+import gptImgLogo from "../../../../../public/./ChatGPT_logo.svg.png";
+
 export const SideBar = () => {
   return (
     <>
       <div className="sidebar">
         <div className="upperSide">
           <div className="upperSideTop">
-            <Image src={chatGPTLogo} alt="" className="logo" />
-            <span className="brand">ChatGPT</span>
-
-            <button className="midBtn">
-              <Image src="" alt="" className="addBtn" />
-              New Chat
+            <div className="mb-8">
+              <Image src={chatGPTLogo} alt="" className="logo inline-block" />
+              <span className="brand">ChatGPT</span>
+            </div>
+            <button className="midBtn flex items-center">
+              <Image src={addBtn} alt="" className="addBtn" />
+              <span>New Chat</span>
             </button>
             <div className="upperSideBarBottom">
-              <button className="query">
-                <Image src="" alt="" />
+              <button className="query flex items-center">
+                <Image src={msgIcon} alt="" />
                 What is Programming ?
               </button>
-              <button className="query">
-                <Image src="" alt="" />
-                What is Programming ?
+              <button className="query flex items-center">
+                <Image src={msgIcon} alt="" />
+                How to use an API ?
               </button>
             </div>
           </div>
         </div>
-        <div className="lowerSide"></div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <Image src={home} alt="" className="listitemsImg" />
+            Home
+          </div>
+          <div className="listItems">
+            <Image src={saved} alt="" className="listitemsImg" />
+            Saved
+          </div>
+          <div className="listItems">
+            <Image src={rocked} alt="" className="listitemsImg" />
+            Upgrade to PRO
+          </div>
+        </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          <div className="chat">
+            <Image
+              src={userIcon}
+              alt="User"
+              className="w-20 h-20 object-cover chatImg"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              perspiciatis, totam inventore culpa voluptas odio in? Doloremque
+              quaerat eos delectus, velit iste consectetur eveniet sint quasi
+              ipsam necessitatibus, nemo veritatis.
+            </p>
+          </div>
+          <div className="chat bot">
+            <Image src={gptImgLogo} alt="GPT" className="w-20 h-20 chatImg" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              perspiciatis, totam inventore culpa voluptas odio in? Doloremque
+              quaerat eos delectus, velit iste consectetur eveniet sint quasi
+              ipsam necessitatibus, nemo veritatis.
+            </p>
+          </div>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" placeholder="Send a message" />
+            <button className="send">
+              <Image src={sendBtn} alt="Send" />
+            </button>
+          </div>
+          <p>
+            ChatGPT may produce inaccurate information about people, places, or
+            facts. ChatGPT August 20 Version
+          </p>
+        </div>
+      </div>
     </>
   );
 };
